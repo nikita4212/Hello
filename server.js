75ce5a519da4a90295c2fd84ded1385f7793abb3
Nikita <nikita@Nikitas-MacBook-Air.local>
2025-11-20 21:29:30 +0530

    Initial Commit 1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Hello World</title>
+        <title>I Love You Tirth :) ❤️</title>
         <style>
           body {
             margin: 0;
